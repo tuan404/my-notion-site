76e853379a1e87ce97cb28262aa804c58e89c570
tuan404 <55226286+tuan404@users.noreply.github.com>
2023-08-02 00:43:20 +0700

    Update site.config.ts
    Basic site info

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '9964d97e8845414880140eec73c04df6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Howdy, I am Tuan',
+  domain: 'tuantruong.site',
+  author: 'Tuan Truong',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
