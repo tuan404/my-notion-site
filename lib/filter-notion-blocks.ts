cc60bfb7f0a6b81db73a8c12cb75a0828e30d887
tvtuan.404 <tvtuan.404@gmail.com>
2025-02-28 10:00:59 +0700

    Remove debug console logs from filter-notion-blocks

--- lib/filter-notion-blocks.ts
+++ lib/filter-notion-blocks.ts
@@ -6,12 +6,8 @@ import { hiddenBlocks } from './hidden-blocks'
  */
 export function filterHiddenBlocks(recordMap: ExtendedRecordMap): ExtendedRecordMap {
   if (!recordMap?.block || !hiddenBlocks.length) {
-    console.log('No blocks to filter or hiddenBlocks is empty');
     return recordMap
   }
-  
-  console.log('Hidden block IDs:', hiddenBlocks);
-  console.log('Available block IDs:', Object.keys(recordMap.block).slice(0, 5));
 
   // Create a new record map with filtered blocks
   const filteredBlocks = { ...recordMap.block }
@@ -19,10 +15,7 @@ export function filterHiddenBlocks(recordMap: ExtendedRecordMap): ExtendedRecord
   // Remove any blocks that are in the hiddenBlocks array
   hiddenBlocks.forEach(blockId => {
     if (filteredBlocks[blockId]) {
-      console.log(`Filtering block: ${blockId}`);
       delete filteredBlocks[blockId]
-    } else {
-      console.log(`Block ID not found: ${blockId}`);
     }
   })
 
